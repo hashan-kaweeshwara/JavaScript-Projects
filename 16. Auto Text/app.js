a7d2ea7ac468ec29009a87db15da1cc3a6dc1029
Hashan Kaweeshwara <64040946+hashkawee@users.noreply.github.com>
2024-08-22 22:36:04 +0530

    new

--- 16. Auto Text/app.js
+++ 16. Auto Text/app.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursor = document.querySelector(".cursor");
 
-const words = ["Awesome", "Fun", "Cool", "Life", "Famous", "Weird"];
+const words = ["Awesome", "Fun", "Cool", "Life", "Famous", "Weird", "👌"];
 
 const typingDelay = 200;
 const erasingDelay = 200;
