5ec3e8804709e2c53cfe08b928aa6ec483bab36e
Hashan Kaweeshwara <64040946+hashkawee@users.noreply.github.com>
2024-09-05 23:39:32 +0530

    new

--- 00. Mini Projects/app.js
+++ 00. Mini Projects/app.js
@@ -1,1 +1,9 @@
 console.log("JS Loaded");
+
+var countdownElement = document.getElementById("countdown");
+
+var countDownVal = countdownElement.innerHTML;
+
+setInterval(function () {
+  countdownElement.innerHTML = countDownVal - 1;
+}, 1000);
