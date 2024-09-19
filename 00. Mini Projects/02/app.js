3ae2606ec4fbf9ef0f0a1f051fb99df6c4d5f3b0
Hashan Kaweeshwara <64040946+hashkawee@users.noreply.github.com>
2024-09-19 23:33:07 +0530

    new

--- 00. Mini Projects/02/app.js
+++ 00. Mini Projects/02/app.js
@@ -5,5 +5,5 @@ var initialCountdownVal = countdownElement.innerHTML;
 setInterval(function () {
   initialCountdownVal = initialCountdownVal > 0 ? initialCountdownVal - 1 : 0;
   countdownElement.innerHTML = initialCountdownVal;
-  countdownElement.style.fontSize = "300px";
+  countdownElement.style.fontSize = initialCountdownVal * 100 + "px";
 }, 1000);
