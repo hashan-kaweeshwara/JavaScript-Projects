1983b8eec7f482a359184a766eedfa8562dc93bc
Hashan Kaweeshwara <64040946+hashan-kaweeshwara@users.noreply.github.com>
2024-10-11 16:14:40 +0530

    new

--- 00. Mini Projects/03/app.js
+++ 00. Mini Projects/03/app.js
@@ -1,4 +1,5 @@
 var btn = document.getElementById("btn-click");
+var mainDiv = document.querySelector("body");
 
 // function onBtnClick() {
 //   alert("Button Clicked");
@@ -7,3 +8,44 @@ var btn = document.getElementById("btn-click");
 // btn.onclick = onBtnClick;
 
 // btn.addEventListener("click", onBtnClick);
+
+function onBtnClick() {
+  //   alert(
+  //     "rgb(" +
+  //       Math.floor(Math.random() * 255) +
+  //       "," +
+  //       Math.floor(Math.random() * 255) +
+  //       "," +
+  //       Math.floor(Math.random() * 255) +
+  //       ")"
+  //   );
+
+  mainDiv.style.backgroundColor =
+    "rgb(" +
+    Math.floor(Math.random() * 255) +
+    "," +
+    Math.floor(Math.random() * 255) +
+    "," +
+    Math.floor(Math.random() * 255) +
+    ")";
+
+  btn.style.backgroundColor =
+    "rgb(" +
+    Math.floor(Math.random() * 255) +
+    "," +
+    Math.floor(Math.random() * 255) +
+    "," +
+    Math.floor(Math.random() * 255) +
+    ")";
+
+  btn.style.color =
+    "rgb(" +
+    Math.floor(Math.random() * 255) +
+    "," +
+    Math.floor(Math.random() * 255) +
+    "," +
+    Math.floor(Math.random() * 255) +
+    ")";
+}
+
+btn.onclick = onBtnClick;
