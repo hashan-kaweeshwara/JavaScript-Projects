c1c2f6f19efbb0b5310f2f21bad8fa3a82072a4a
Hashan Kaweeshwara <64040946+hashan-kaweeshwara@users.noreply.github.com>
2024-10-18 22:06:23 +0530

    new

---
+++ 00. Mini Projects/05/app.js
@@ -0,0 +1,6 @@
+var mainHeading = document.getElementById("main-heading");
+var btnDecFont = document.getElementById("btn-dec");
+var btnincFont = document.getElementById("btn-inc");
+
+console.log(window.getComputedStyle(btnDecFont));
+console.log(btnincFont.style.marginRight);
