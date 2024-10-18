7bac8f9bc6ff2e246199fceb25d1bfef12854a9a
Hashan Kaweeshwara <64040946+hashan-kaweeshwara@users.noreply.github.com>
2024-10-18 22:20:21 +0530

    new

--- 00. Mini Projects/05/app.js
+++ 00. Mini Projects/05/app.js
@@ -2,5 +2,17 @@ var mainHeading = document.getElementById("main-heading");
 var btnDecFont = document.getElementById("btn-dec");
 var btnincFont = document.getElementById("btn-inc");
 
-console.log(window.getComputedStyle(btnDecFont));
-console.log(btnincFont.style.marginRight);
+var initialFontSize = window.getComputedStyle(mainHeading).fontSize;
+initialFontSize = parseInt(
+  initialFontSize.substring(0, initialFontSize.length - 2)
+);
+
+btnincFont.onclick = function () {
+  initialFontSize += 10;
+  mainHeading.style.fontSize = initialFontSize + "px";
+};
+
+btnDecFont.onclick = function () {
+  initialFontSize -= 10;
+  mainHeading.style.fontSize = initialFontSize + "px";
+};
