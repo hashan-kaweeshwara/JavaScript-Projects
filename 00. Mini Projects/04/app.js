213e571f2b4fb84ac1f2b3aade404d94bb81bb50
Hashan Kaweeshwara <64040946+hashan-kaweeshwara@users.noreply.github.com>
2024-12-19 11:12:12 +0530

    new

--- 00. Mini Projects/04/app.js
+++
@@ -1,8 +0,0 @@
-var mainHeading = document.getElementById("main-heading");
-var btn = document.getElementById("btn-click");
-
-mainHeading.classList.add("big");
-
-btn.addEventListener("click", function () {
-  mainHeading.classList.remove("big");
-});
